a025fb88cdbc4735e582325ee053009b3ecd69df
AkaashSamson <akaashsam22@gmail.com>
2024-08-20 09:53:38 +0530

    touch up and sjf attempts - test phase

--- Os-expt3/sjf.cpp
+++ Os-expt3/sjf.cpp
@@ -2,6 +2,7 @@
 
 using namespace std;
 
+#define MAX 100
 class process{
 
 public:
@@ -20,10 +21,56 @@ public:
     }
 
     void display(){
-        cout<<" P"<<no<<"\t\t\t"<<bt<<"\t\t"<<at<<"\t\t"<<tat<<"\t\t"<<wt<<"\t\t"<<ct<<"\n";
+         cout<<" P"<<no<<"\t\t\t"<<at<<"\t\t"<<bt<<"\t\t"<<ct<<"\t\t"<<tat<<"\t\t"<<wt<<"\n";
     }
 
 };
+
+process *rq = new process[MAX];
+
+//making rq as efficientt queue for process with insert, delete, isfull is empty and sort by bt functions
+int front=-1,rear=-1;
+
+int isfull(){
+if ((front == rear + 1) || (front == 0 && rear == MAX-1))
+   return 1;
+else
+   return 0;
+}
+
+int isempty(){
+if (front == -1)
+   return 1;
+else
+   return 0;
+}
+
+void insert(process p){
+
+if(isfull())
+    cout<<"Queue is full\n";
+else{
+    if(front == -1)
+        front = 0;
+    rear = (rear + 1) % MAX;
+    rq[rear] = p;
+}
+}
+
+process del(){
+if(isempty())
+    cout<<"Queue is empty\n";
+else{
+    process p = rq[front];
+    if(front == rear){
+        front = -1;
+        rear = -1;
+    }
+    else
+        front = (front + 1) % MAX;
+    return p;
+}
+}
 
 void sort_process(class process *p, int n){
 class process temp;
@@ -43,7 +90,7 @@ for(int i=0; i<n-1; i++){
 }
 }
 
-void sort_process(class process *p, int n){
+void sort_bt(class process *p, int n){
 class process temp;
 int xchange=0;
 for(int i=0; i<n-1; i++){
@@ -81,31 +128,38 @@ for(int i=0; i<n-1; i++){
 
 
 void sjf(int n, class process *p){
-process *rq = new process[n];
 sort_process(p,n);
-int i,tat=0,wt=0,t=0,tstmp[100],ind=0;
-if(p[0].at!=0)
-tstmp[ind++]=-1;
-tstmp[ind++]=0;
-t = p[0].at+p[0].bt;
-    p[0].tat=p[0].bt-p[0].at;
-    p[0].ct = t;
-    p[0].wt = 0;
+int i,tat=0,wt=0,t=0,tstmp[100],ind=0,ptr=0;
+insert(p[0]);
 
-    for(i=1;i<n;i++){
-if (t < p[i].at){
-t += p[i].at - t;
-tstmp[ind++]=-1;
-}
-tstmp[ind++]=i;
-        p[i].ct = t + p[i].bt;
-t=p[i].ct;
-        p[i].tat = p[i].ct-p[i].at;
-        p[i].wt=p[i].tat-p[i].bt;
+while(!isempty() || ptr<n){
+    if(isempty()){
+        insert(p[ptr++]);
     }
+    i = del().no-1;
+    cout<<t<<" "<<p[i].bt<<" ";
+    if(t<p[i].at){
+        tstmp[ind++]=-1;
+        t = p[i].at;
+    }
+    tstmp[ind++]=i;
+    cout<<t<<" ";
+    t += p[i].bt;
+    p[i].ct = t;
+    cout<<p[i].ct<<"\n";
+    p[i].tat = p[i].ct-p[i].at;
+    p[i].wt = p[i].tat-p[i].bt;
+    tat+=p[i].tat;
+    wt+=p[i].wt;
+    while(ptr<n && p[ptr].at<=t){
+        insert(p[ptr]);
+        ptr++;
+    }
+    sort_bt(rq,rear+1);
+}
 
-     cout<<"Gantt Chart\n |";
-    for(i=0;i<ind;i++){
+cout<<"Gantt Chart\n |";
+for(i=0;i<ind;i++){
 if(tstmp[i]==-1 && i==0)
 	continue;
 if(tstmp[i]==-1)
@@ -130,17 +184,13 @@ else
     }
     cout<<"\n";
 
-
-    for(i=0;i<n;i++){
-        tat+=p[i].tat;
-        wt+=p[i].wt;
-    }
     sort_id(p,n);
 
-    cout<<"Process \tBurst Time \tArrival Time \tTurnaround Time \tWaiting Time\t CT \n";
+    cout<<"Process \tArrival Time \tBurst Time \tCompletion Time     Turnaround Time   Waiting Time \n";
     for(i=0;i<n;i++){
         p[i].display();
     }
+
 
     cout<<"Average Turnaround Time: "<<(float)tat/n<<"\n";
     cout<<"Average Waiting Time: "<<(float)wt/n<<"\n";
@@ -157,14 +207,18 @@ process *p = new process[n];
 cout<<"are the arrival times provided?(1 for yes/ 0 for no):";
 cin>>f;
 
+cout<<"Enter the burst time of processes:";
 for(i=0;i<n;i++){
-cout<<"Enter the burst time of process p"<<i+1<<":";
-cin>>b;
+cin>>p[i].bt;
+p[i].no=i+1;
+
+}
+ cout<<"Enter the arrival time of processes:";
 if(f==1){
-    cout<<"Enter the arrival time of process p"<<i+1<<":";
-    cin>>a;
-}                                                                                                        
-p[i].getval(i+1,b,a);
+   for(i=0;i<n;i++){
+cin>>p[i].at;
+}
+
 }
 
 sjf(n,p);   
